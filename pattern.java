6f9728ef1f59e988852ddf7f853a577dc88c6592
Abdullah <abdullahaziz723@gmail.com>
2025-01-31 15:20:31 +0530

    practice

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern {
+
+    public static void main(String[] args) {
+        // int row,col=0;
+        for(int row =0;row<5;row++){
+            System.out.print( "");
+            for(int col=1;col>row;col++){
+
+                System.out.print("*");
+            }
+        }
+    }
+}
