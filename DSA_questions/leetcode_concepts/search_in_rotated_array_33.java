f5cc81758e439fbf686cb5bfdb761751e48e110e
abdullah <abdullahaziz723@gmail.com>
2025-07-08 23:25:48 +0530

    random

--- DSA_questions/leetcode_concepts/search_in_rotated_array_33.java
+++ DSA_questions/leetcode_concepts/search_in_rotated_array_33.java
@@ -1,6 +1,38 @@
 public class search_in_rotated_array_33 {
 
     public static void main(String[] args) {
-        
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int result = search(arr, target);
+        System.out.println("Index of " + target + " is: " + result);
+        // Output: Index of 0 is: 4
+    }
+    static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // Target is in the left half
+                } else {
+                    left = mid + 1; // Target is in the right half
+                }
+            } else { // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // Target is in the right half
+                } else {
+                    right = mid - 1; // Target is in the left half
+                }
+            }
+        }
+
+        return -1; // Target not found
     }
 }
