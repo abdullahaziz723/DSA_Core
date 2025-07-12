df7ae76f9aa832e7eb05e8d0630b5557e3f8cd83
abdullah <abdullahaziz723@gmail.com>
2025-07-12 20:14:54 +0530

    pyramid

--- DSA_questions/patterns/inverted_half_pyramid.java
+++ DSA_questions/patterns/inverted_half_pyramid.java
@@ -1,3 +1,12 @@
 public class inverted_half_pyramid {
-    
-}
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j =4;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+            
+        }
+    }
+
