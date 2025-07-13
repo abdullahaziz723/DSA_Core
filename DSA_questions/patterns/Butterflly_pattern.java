6ad5c088e03222dfc58429c7bd681d52456759c7
abdullah <abdullahaziz723@gmail.com>
2025-07-13 23:15:51 +0530

    string Builder

---
+++ DSA_questions/patterns/Butterflly_pattern.java
@@ -0,0 +1,12 @@
+public class Butterflly_pattern {
+    public static void main(String[] args) {
+        
+        String str = "abdullah";
+        String str1="xbdullah";
+
+       StringBuilder sb = new StringBuilder("tony");
+       System.out.println(sb.reverse());
+
+    }
+    
+}
