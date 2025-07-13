406137721d6ddae15939bd570edf226d18b9ad8e
abdullah <abdullahaziz723@gmail.com>
2025-07-13 23:17:48 +0530

    methods

--- DSA_questions/patterns/Butterflly_pattern.java
+++ DSA_questions/patterns/Butterflly_pattern.java
@@ -5,7 +5,14 @@ public class Butterflly_pattern {
         String str1="xbdullah";
 
        StringBuilder sb = new StringBuilder("tony");
-       System.out.println(sb.reverse());
+    //    System.out.println(sb.reverse());
+
+        ///cha atAtIndex
+        System.out.println(sb.charAt(2));
+        //set char at index 0
+       sb.setCharAt(0, 'a');
+       System.out.println(sb);
+
 
     }
     
