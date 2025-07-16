566163636bc38276ab239b130f14c165b377cc4c
abdullah <abdullahaziz723@gmail.com>
2025-07-17 00:53:47 +0530

    recur!@%1

--- DSA_questions/Recursion/recursion5.java
+++ DSA_questions/Recursion/recursion5.java
@@ -1,3 +1,30 @@
+package Recursion;
 public class recursion5 {
-    
+ public static void main(String[] args) {
+    int x =2,n=5;
+    int ans =calpower(x, n);
+    System.out.println(ans);
+
+ }  
+ 
+ 
+  public static int calpower( int x,int n){
+   if(n==0){
+    return 1 ;
+   }
+   if(x==0)
+   return 0;
+
+   
+    // int xpownm1=calpower(x, n-1) ;
+    // int xpow=x*xpownm1;
+    // return xpow;
+
+    //for logn height recursion
+    if(n%2==0){
+       return  calpower(x, n/2)*calpower(x, n/2);    
+    }else{
+       return x*calpower(x, n/2)*calpower(x, n/2);
+}
+  }
 }
