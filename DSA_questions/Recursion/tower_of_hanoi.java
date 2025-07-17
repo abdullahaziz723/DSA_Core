53fc5ddc4abe0a6682896338271513495b60f081
abdullah <abdullahaziz723@gmail.com>
2025-07-17 14:31:27 +0530

    tpwerofHanoi

---
+++ DSA_questions/Recursion/tower_of_hanoi.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class tower_of_hanoi {
+
+    public static void towerOfHanoi(int n ,String src,String helper,String dest){
+        if(n==1){
+            System.out.println("transfer disk "+n+" from " +src +" to "+ dest);
+            return ; // Corrected typo from 'rerturn' to 'return'
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+        System.out.println("transfer disk "+n+" from " +src +" to "+ dest);
+        towerOfHanoi(n-1, helper, src, dest);
+    }
+    public static void main(String[] args) {
+        
+        int n =3;
+        towerOfHanoi(n, "S", "H", "D");
+
+    }
+}
